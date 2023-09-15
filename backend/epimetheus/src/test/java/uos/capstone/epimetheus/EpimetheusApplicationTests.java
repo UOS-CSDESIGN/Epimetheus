6350d596cdddccfcaf12ac99abeab3afbf5e5101
humang <43851194+humang@users.noreply.github.com>
2023-09-15 21:49:15 +0900

    [BE] JIRA-73 #comment springboot server initialize

---
+++ backend/epimetheus/src/test/java/uos/capstone/epimetheus/EpimetheusApplicationTests.java
@@ -0,0 +1,13 @@
+package uos.capstone.epimetheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpimetheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
