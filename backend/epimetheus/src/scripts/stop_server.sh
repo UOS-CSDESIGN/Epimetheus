4804e3001d33b36ecf83057155c63fbc6ec68c9d
김종원 Jongwon <tank3a@gmail.com>
2023-10-12 09:52:58 +0900

    JIRA-42 #bug_fix #comment "CodeDeploy 실행 스크립트 디렉토리 위치 오류 해결"

--- backend/epimetheus/src/scripts/stop_server.sh
+++ backend/epimetheus/src/scripts/stop_server.sh
@@ -1,7 +1,8 @@
-PROJECT_ROOT="/home/ubuntu"
+PROJECT_ROOT="/home/ubuntu/jenkins"
 JAR_FILE="$PROJECT_ROOT/epimetheus.jar"
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 이전에 실행중인 SpringBoot Server가 존재하지 않습니다." >> $DEPLOY_LOG
