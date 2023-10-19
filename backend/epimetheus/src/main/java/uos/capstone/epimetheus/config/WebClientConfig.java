7f0105581e43820b9027e54e4c4b194d8e06483f
humang <43851194+humang@users.noreply.github.com>
2023-10-19 21:50:04 +0900

    JIRA-93 #feat #comment "Llama 응답 timeout 설정"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/config/WebClientConfig.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/config/WebClientConfig.java
@@ -1,17 +1,34 @@
 package uos.capstone.epimetheus.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
+    HttpClient httpClient = HttpClient.create()
+            .responseTimeout(Duration.ofSeconds(180))
+            .tcpConfiguration(
+                    client -> client.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 50000)
+                            .doOnConnected(
+                                    conn -> conn.addHandlerFirst(new ReadTimeoutHandler(500))
+                                            .addHandlerLast(new WriteTimeoutHandler(6000))
+                            )
+            );
+
+
     @Bean
     public WebClient webClient() {
-        return WebClient.create();
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
-
-
-
 }
