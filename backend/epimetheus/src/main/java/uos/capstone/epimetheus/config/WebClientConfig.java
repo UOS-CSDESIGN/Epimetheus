e0fbf018ff7f894ee28cf42783b570337b71d181
humang <43851194+humang@users.noreply.github.com>
2023-10-19 21:50:04 +0900

    JIRA-93 #feat #comment "Llama 응답 timeout 설정"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/config/WebClientConfig.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/config/WebClientConfig.java
@@ -1,6 +1,8 @@
 package uos.capstone.epimetheus.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
