e8be25cf2dd3a6882d58dbc7a56c5e218cd47505
CsLsj <43843214+CsLsj@users.noreply.github.com>
2023-10-19 01:40:19 +0000

    Update and rename getQuery.tsx to GetQuery.tsx

--- frontend/src/api/GetQuery.tsx
+++ frontend/src/api/GetQuery.tsx
@@ -1,31 +1,5 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@tanstack/react-query';
-
-const baseURL = 'http://localhost:8000';
+import React from 'react';
 
 export default function GetQuery() {
-    const { data, error, isLoading } = useQuery({
-        queryKey: ['cityName'],
-        queryFn: async () => {
-            const response = await fetch('http://localhost:8000/cities');
-            const data = await response.json();
-            return data;
-        },
-    });
-    useEffect(() => {
-        if (data) console.log(data);
-    }, [data]);
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error: </div>;
-    }
-
-    return (
-        <div>
-            <p>{data.cityName}</p>
-        </div>
-    );
+    return <div>getQuery</div>;
 }
