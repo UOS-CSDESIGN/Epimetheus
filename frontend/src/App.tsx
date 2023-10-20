1465658a13ebdfbaf0d94bd446af3252582b727a
김종원 Jongwon <tank3a@gmail.com>
2023-10-20 19:49:56 +0900

    Revert "FE - 기초 세팅 (#34)"
    This reverts commit 541cd100b1d79949bf42d3cfcf642def87bdebd7.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,5 @@
-import React, { useContext, useState } from 'react';
-import ConsolePage from './pages/ConsolePage';
-import styled from 'styled-components';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const AppDiv = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-color: #ffffff;
-`;
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 0,
-        },
-    },
-});
+import React from 'react';
 
 export default function App() {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <AppDiv>
-                <ConsolePage />
-            </AppDiv>
-        </QueryClientProvider>
-    );
+    return <div>App</div>;
 }
