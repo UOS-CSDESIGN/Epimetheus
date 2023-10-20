14ae2960eb88287a0d0a3b5ddcbd8a1ceb4cf6da
humang <43851194+humang@users.noreply.github.com>
2023-10-20 22:07:16 +0900

    JIRA-60 #feat #comment "Task 요청 API 구현"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/EpimetheusApplication.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/EpimetheusApplication.java
@@ -2,8 +2,10 @@ package uos.capstone.epimetheus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/application.yml")
 public class EpimetheusApplication {
 
 	public static void main(String[] args) {
