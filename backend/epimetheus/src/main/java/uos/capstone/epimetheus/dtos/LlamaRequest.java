766a8c38e2cf5b905675599096297f77069bfc9c
humang <43851194+humang@users.noreply.github.com>
2023-10-21 14:59:06 +0900

    JIRA-93 #close #comment "수정시 빠진 부분 재수정"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/dtos/LlamaRequest.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/dtos/LlamaRequest.java
@@ -8,14 +8,14 @@ import java.util.List;
 @Getter
 public class LlamaRequest {
 
-    private final List<LlamaRequestMessage> llamaRequestMessages;
+    private final List<LlamaRequestMessage> messages;
     private final int temperature;
     private final int max_tokens;
     private final boolean stream;
 
     @Builder
-    public LlamaRequest(List<LlamaRequestMessage> llamaRequestMessages, int temperature, int max_tokens, boolean stream) {
-        this.llamaRequestMessages = llamaRequestMessages;
+    public LlamaRequest(List<LlamaRequestMessage> messages, int temperature, int max_tokens, boolean stream) {
+        this.messages = messages;
         this.temperature = temperature;
         this.max_tokens = max_tokens;
         this.stream = stream;
