5828b6bf0ca7635562311b450c2e563bc54beb96
humang <43851194+humang@users.noreply.github.com>
2023-10-21 14:40:45 +0900

    JIRA-60 #close #comment "불필요한 부분 제거"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/EpimetheusApplication.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/EpimetheusApplication.java
@@ -2,10 +2,8 @@ package uos.capstone.epimetheus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:/application.yml")
 public class EpimetheusApplication {
 
 	public static void main(String[] args) {
