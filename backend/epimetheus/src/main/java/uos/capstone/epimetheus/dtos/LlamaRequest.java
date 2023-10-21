ec78343457d742100e07f51885f5de248e2437a1
humang <43851194+humang@users.noreply.github.com>
2023-10-21 14:49:05 +0900

    JIRA-93 #close #comment "직관적이지 않은 이름 수정"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/dtos/LlamaRequest.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/dtos/LlamaRequest.java
@@ -8,14 +8,14 @@ import java.util.List;
 @Getter
 public class LlamaRequest {
 
-    private final List<Message> messages;
+    private final List<LlamaRequestMessage> llamaRequestMessages;
     private final int temperature;
     private final int max_tokens;
     private final boolean stream;
 
     @Builder
-    public LlamaRequest(List<Message> messages, int temperature, int max_tokens, boolean stream) {
-        this.messages = messages;
+    public LlamaRequest(List<LlamaRequestMessage> llamaRequestMessages, int temperature, int max_tokens, boolean stream) {
+        this.llamaRequestMessages = llamaRequestMessages;
         this.temperature = temperature;
         this.max_tokens = max_tokens;
         this.stream = stream;
