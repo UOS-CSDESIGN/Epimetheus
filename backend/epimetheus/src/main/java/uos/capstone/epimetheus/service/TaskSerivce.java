26c00b7a555ef686b624b5bd2584327524a1d6f7
humang <43851194+humang@users.noreply.github.com>
2023-10-25 21:14:02 +0900

    JIRA-91 #feat #comment "클라이언트의 code를 저장하는 api 구성 완료"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/service/TaskSerivce.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/service/TaskSerivce.java
@@ -1,9 +1,12 @@
 package uos.capstone.epimetheus.service;
 
 import reactor.core.publisher.Flux;
+import uos.capstone.epimetheus.dtos.TaskStep;
 import uos.capstone.epimetheus.dtos.llamaTasks.SubTaskResolver;
 
 public interface TaskSerivce {
 
     Flux<SubTaskResolver> getSubTaskListInStream(String task);
+
+    String saveCode(TaskStep taskStep);
 }
