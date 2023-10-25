b2fcd8073a5f94b3ae0c9c967944c6b9f795343e
humang <43851194+humang@users.noreply.github.com>
2023-10-25 21:15:12 +0900

    JIRA-37 #feat #comment "전체 http메소드 cors설정 완료"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/config/WebClientConfig.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package uos.capstone.epimetheus.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
