efd89854e4069d4f98c2a48f5a1357ca65201a33
김종원 Jongwon <tank3a@gmail.com>
2023-10-31 14:59:12 +0900

    JIRA-134 #feat #comment "Step을 Embedding Vector로 바꾸기 위한 DTO 추가 및 float[] 추가"

---
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/dtos/LlamaVectorRequest.java
@@ -0,0 +1,26 @@
+package uos.capstone.epimetheus.dtos;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class LlamaVectorRequest {
+
+    private List<EmbeddingData> data;
+    private TokenUsage usage;
+}
+
+@Getter
+class EmbeddingData {
+
+    private String object;
+    private String embedding;
+    private String index;
+}
+
+@Getter
+class TokenUsage {
+    private int prompt_tokens;
+    private int total_tokens;
+}
