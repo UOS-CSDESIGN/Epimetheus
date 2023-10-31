7558309644d98e9e0ca89d6cb6c1cb498bd2b1b3
김종원 Jongwon <tank3a@gmail.com>
2023-10-31 15:42:25 +0900

    JIRA-134 #refac #comment "새로운 Llama API 추가로 인한 기존 코드의 명칭 개선"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/dtos/LlamaVectorResponse.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/dtos/LlamaVectorResponse.java
@@ -1,6 +1,5 @@
 package uos.capstone.epimetheus.dtos;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 
 import java.util.List;
@@ -10,22 +9,14 @@ public class LlamaVectorResponse {
 
     private List<EmbeddingData> data;
     private TokenUsage usage;
-
-    public double[] getVector() {
-        if((data != null ? data.size() : 0) != 1) {
-            throw new RuntimeException("Invalid Data Came");
-        }
-
-        return data.get(0).getEmbedding();
-    }
 }
 
 @Getter
 class EmbeddingData {
 
     private String object;
-    private double[] embedding;
-    private int index;
+    private String embedding;
+    private String index;
 }
 
 @Getter
