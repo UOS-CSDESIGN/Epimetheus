2f8b4a6aa62da2d9aa6f41a61b06bb64a7ce1ac3
seongho9 <seongho9276@gmail.com>
2023-11-01 15:28:56 +0900

    JIRA-84 #feat #comment "ajax to get code and post code"

--- frontend/src/pages/CodeInputPage.tsx
+++ frontend/src/pages/CodeInputPage.tsx
@@ -1,75 +1,56 @@
 import React from 'react'
 import CodeInputComponent, { CodeInputProps } from '../components/CodeInputComponent';
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import LanguageSelectComponent from '../components/LanguageSelectComponent';
+import { HiChevronDown } from 'react-icons/hi';
+import SubTaskComponent from '../components/SubTaskComponent';
+import {CodeInputP, CodeInput, CodeInputLayer, SwitchButton, SubmitButton } from './CodeInputPageStyle'
 
-const CodeInputP = styled.div`
-    display: flex;
-    flex-direction: column;
-    allign-items: center;
-    width: 100vw;
-    height: auto;
-    margin: 0;
-    padding: 0;
-    border: 0;
-    background-color: #ffffff;
-`;
-const CodeInputLayer = styled.div`
-
-    background-color: #f0f0f0;
-    width: 90vw;
-
-    min-height: 80vh;
-    max-height: auto;
-    
-    margin-top: 5vh;
-    margin-bottom: 5vh;
-    margin-left: 5vw;
-    margin-right: 5vw;
-
-    padding-top: 0vh;
-    padding-bottom: 0vh;
-    padding-left: 0vw;
-    padding-right: 0vw;
-
-    border: none;
-    border-radius: 20px;
-
-    overflow: inherit;
-    text-overflow: ellipsis;
-
-`;
-const CodeInput = styled.div`
-    background-color: #ffffff;
-    width: 80vw;
-    min-height: 69vh;
-    max-height: auto;
-    
-    margin-left: 5vw;
-    margin-right: 5vw;
-    margin-top: 1vh;
-    margin-bottom: 1vh;
-    border-radius: 20px;
-    padding: 0;
-
-    filter: drop-shadow(4px 4px 10px rgba(54, 54, 54, 0.25))
-        drop-shadow(-4px -4px 4px rgba(255, 255, 255, 0.25));
-`;
-export default function CodeINputPage() {
+export default function CodeInputPage() {
 
     const [lang, setLanguage] = useState<string>("python");
+    const [isVisible, setIsVisible] = useState<boolean>(true);
+    const targetRef = useRef<number>(0);
     
     const onSelect =(e: React.ChangeEvent<HTMLSelectElement>) =>{
         setLanguage(e.target.value);
     }
+    useEffect(()=>{
+        //bring code
+        //brinng tasks
+        const timer = setInterval(()=>{
+            window.addEventListener("scroll", handleScroll);
+        },1000);
+        return(()=>{
+            clearInterval(timer);
+            window.removeEventListener("scroll", handleScroll);
+        })
+    }, []);
+    
+    const handleScroll = () =>{
+        if(window.scrollY>targetRef.current){
+            setIsVisible(false);
+        } else{
+            setIsVisible(true);
+        }
+        targetRef.current = window.scrollY;
+        console.log(targetRef.current);
+    }
+    const onSubmit = () =>{
+        window.scrollTo(0,document.body.scrollHeight);
+    }
   return (
     <CodeInputP>
+        <SwitchButton isVisible={isVisible} position={targetRef.current} onClick={onSubmit}>
+            <HiChevronDown/>
+        </SwitchButton>
         <CodeInputLayer>
             <LanguageSelectComponent onChange={onSelect}/>
             <CodeInput>
                 <CodeInputComponent language={lang}/>
             </CodeInput>
+            <SubmitButton>ㄱㄱ</SubmitButton>
         </CodeInputLayer>
     </CodeInputP>
   );
