afd653266f2ce13e8c46162dc65840a4ca2c49f9
CsLsj <43843214+CsLsj@users.noreply.github.com>
2023-11-02 21:48:27 +0900

    Update TaskCodeViewComponent.tsx

--- frontend/src/components/TaskCodeViewComponent.tsx
+++ frontend/src/components/TaskCodeViewComponent.tsx
@@ -15,29 +15,6 @@ interface TaskCodeProps {
     code: string[];
 }
 
-const CodeBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-`;
-
-const AnswerDiv = styled.textarea`
-    font-size: 1.6rem;
-    width: 96%;
-    resize: none;
-    height: auto;
-    line-height: 3rem;
-`;
-
-const CodeButton = styled.button`
-    border: thin solid #d6d6d6;
-    width: 4rem;
-    height: 3rem;
-    border-bottom-left-radius: 2rem;
-    border-bottom-right-radius: 2rem;
-`;
-
 export default function TaskCodeViewComponent(props: TaskCodeProps) {
     return (
         <CodeBox>
