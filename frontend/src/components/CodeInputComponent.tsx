ae9926ca267a8573f13481491f2ee6fb023fa9a2
CsLsj <43843214+CsLsj@users.noreply.github.com>
2023-11-04 11:02:31 +0900

    BugFix and Refactor Code

--- frontend/src/components/CodeInputComponent.tsx
+++ frontend/src/components/CodeInputComponent.tsx
@@ -1,68 +1,12 @@
 import { useContext, useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/atom-one-dark.css';
 import { StateContext } from '../App';
-
-const CodeEditor = styled.div`
-    width: 80vw;
-    min-height: 70vh;
-    max-height: auto;
-    overflow: inherit;
-    position: relative;
-    border-radius: 20px;
-    font-size: 1rem;
-    margin: 0;
-`;
-const CodeInput = styled.textarea`
-    position: absolute;
-    margin-top: 0vh;
-    margin-right: 0vw;
-    margin-bottom: 2.7%;
-    margin-left: 2vw;
-    padding: 0;
-    width: 100%;
-    min-height: 80vh;
-    max-height: auto;
-    border-radius: 0.25rem;
-    caret-color: $alert;
-    color: transparent;
-    background-color: transparent;
-    z-index: 1;
-    border: none;
-    resize: none;
-    overflow: hidden;
-    &:focus {
-        outline: none;
-    }
-`;
-const Present = styled.pre`
-    background-color: #fff;
-    width: 76vw;
-
-    min-height: 70vh;
-    max-height: auto;
-
-    margin-top: 0;
-    margin-right: 0;
-    margin-bottom: 0;
-    margin-left: 0;
-
-    padding-bottom: 2vh;
-    padding-top: 0vh;
-    padding-left: 2vw;
-    padding-right: 2vw;
-
-    border: none;
-    border-radius: 20px;
-
-    overflow-y: inherit;
-    overflow-x: inherit;
-    text-overflow: ellipsis;
-
-    color: #000000;
-    z-index: 0;
-`;
+import {
+    CodeEditor,
+    CodeInput,
+    Present,
+} from '../styles/CodIenputComponent.styles';
 
 export interface CodeInputProps {
     language: string;
