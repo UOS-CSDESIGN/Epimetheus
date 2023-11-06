086b3934e01e50bce5da6c0b4f4d2e5a6e5bd931
humang <43851194+humang@users.noreply.github.com>
2023-11-06 19:25:17 +0900

    JIRA-39 #test #comment "TaskControllerTest 구현 및 필드명 수정"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/dtos/llamaTasks/SubTaskOutro.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/dtos/llamaTasks/SubTaskOutro.java
@@ -3,23 +3,29 @@ package uos.capstone.epimetheus.dtos.llamaTasks;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @NoArgsConstructor
 public class SubTaskOutro implements SubTaskResolver {
+    @Override
+    public int hashCode() {
+        return Objects.hash(stepId, wrapper, property);
+    }
 
-    int stepNo;
+    int stepId;
     String wrapper;
     ResponseStreamProperty property;
 
     @Builder
-    public SubTaskOutro(int stepNo, String wrapper, ResponseStreamProperty property) {
-        this.stepNo = stepNo;
+    public SubTaskOutro(int stepId, String wrapper, ResponseStreamProperty property) {
+        this.stepId = stepId;
         this.wrapper = wrapper;
         this.property = property;
     }
 
     @Override
-    public int getStepNo() {
-        return stepNo;
+    public int getStepId() {
+        return stepId;
     }
 
     public String getWrapper() {
