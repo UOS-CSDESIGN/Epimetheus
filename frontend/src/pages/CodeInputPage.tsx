07c684d8e8d3c6a0d1f49aebf4a6a84b960b92be
seongho9 <seongho9276@gmail.com>
2023-11-07 22:13:49 +0900

    JIRA-87 #feat #commenet "redirect codeinput page"

--- frontend/src/pages/CodeInputPage.tsx
+++ frontend/src/pages/CodeInputPage.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CodeInputComponent, {
     CodeInputProps,
 } from '../components/CodeInputComponent';
-import styled from 'styled-components';
 import { useState, useEffect, useRef } from 'react';
 import LanguageSelectComponent from '../components/LanguageSelectComponent';
 import { HiChevronDown } from 'react-icons/hi';
@@ -13,13 +12,31 @@ import {
     CodeInputLayer,
     SwitchButton,
     SubmitButton,
+    SubtaskDiv,
 } from '../styles/CodeInputPage.styles';
+import { StateContext } from '../App';
+import { useSearchParams } from 'react-router-dom';
 
 export default function CodeInputPage() {
     const [lang, setLanguage] = useState<string>('python');
     const [isVisible, setIsVisible] = useState<boolean>(true);
     const targetRef = useRef<number>(0);
 
+    const {
+        isLoading,
+        title,
+        description,
+        openCode
+    } = useContext(StateContext);
+
+    const [serchParams] = useSearchParams();
+    const [ref, setRef] = useState<string | null>(serchParams.get('info'));
+    const stepId = useRef<string>(' ');
+    if(serchParams.get('info') === null) {
+        stepId.current = ' ';
+    } else {
+        stepId.current = serchParams.get('info') as string;
+    }
     const onSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setLanguage(e.target.value);
     };
@@ -61,6 +78,9 @@ export default function CodeInputPage() {
                 <HiChevronDown />
             </SwitchButton>
             <CodeInputLayer>
+                <SubtaskDiv>
+                    <SubTaskComponent title={title[stepId.current]} description={description[stepId.current]} isLoading={isLoading[stepId.current]} handleCode={true} />
+                </SubtaskDiv>
                 <LanguageSelectComponent onChange={onSelect} />
                 <CodeInput>
                     <CodeInputComponent language={lang} />
