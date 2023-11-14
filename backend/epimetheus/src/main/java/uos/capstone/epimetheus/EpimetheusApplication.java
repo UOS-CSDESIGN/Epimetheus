938e94552be813bf96bb69ccc06c52e352cfc74c
humang <43851194+humang@users.noreply.github.com>
2023-11-14 23:24:48 +0900

    JIRA-90 #feat #comment "SubTask Validation을 위한 로직 작성"

--- backend/epimetheus/src/main/java/uos/capstone/epimetheus/EpimetheusApplication.java
+++ backend/epimetheus/src/main/java/uos/capstone/epimetheus/EpimetheusApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EpimetheusApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EpimetheusApplication.class, args);
 	}
